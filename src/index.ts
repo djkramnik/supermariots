9abe6f26164b050342b537d50192f194b279180e
David Gurr <davidk.gurr@gmail.com>
2025-01-30 13:53:31 -0500

    grugs choice

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-;(function main() {
+;(async function main() {
   const canvas = document.getElementById('screen') as HTMLCanvasElement
   if (!canvas) {
     console.error('cannot find canvas element')
@@ -11,5 +11,27 @@
   }
 
   context.fillRect(0,0,50,50)
+
+  const tileAsset = await loadAsset({ name: 'tiles' })
+  context.drawImage(tileAsset, 0, 0)
+
+  function loadAsset({
+    name,
+    basePath = '/assets',
+    extension = '.png',
+  }: {
+    name: string
+    basePath?: string
+    extension?: string
+  }): Promise<HTMLImageElement> {
+    return new Promise(resolve => {
+      const image = new Image()
+      image.addEventListener('load', function onLoaded() {
+        image.removeEventListener('load', onLoaded)
+        resolve(image)
+      })
+      image.src = `${basePath}/${name.trim()}${extension}`
+    })
+  }
 })()
 
