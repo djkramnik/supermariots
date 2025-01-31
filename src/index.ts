80de5dfc01fe04291f3071d07d5540646bc7c43e
David Gurr <davidk.gurr@gmail.com>
2025-01-31 16:44:20 -0500

    character sprite sheet

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,15 @@
-import { loadAsset, loadLevel } from "./loaders.js";
-import SpriteSheet from "./sprite.js";
-import { Background } from "./types.js";
+import { loadBackgroundSprites } from "./loaders.js";
 
 ;(async function main() {
   const canvas = document.getElementById('screen') as HTMLCanvasElement
   if (!canvas) {
-    console.error('cannot find canvas element')
+    console.error('Could not find canvas')
     return
   }
-  const context = canvas.getContext('2d')
-  if (!context) {
-    console.error('could not create 2d context?!')
-    return
-  }
-
-  const tileAsset = await loadAsset({ name: 'tiles' })
-  const sprites = new SpriteSheet(tileAsset, 16, 16)
-
-  const level = await loadLevel({ name: '1-1'})
-  console.log('LEVEL:', level)
-
-  sprites.define('ground', 0, 0)
-  sprites.define('sky', 3, 23)
-  // sprites.draw('sky', context, 0, 0) 
-
-  for(const background of level.backgrounds) {
-    drawBackground({
-      background,
-      sprites,
-      context, 
-    })
-  }
-
+  const [sprites, level] = await loadBackgroundSprites({
+    asset: 'tiles',
+    levelName: '1-1',
+    canvas,
+  })
 })()
-
-function drawBackground({
-  background,
-  context,
-  sprites,
-}: {
-  background: Background
-  context: CanvasRenderingContext2D
-  sprites: SpriteSheet
-}) {
-
-  const { tile, ranges } = background
-  for(const range of ranges) {
-    const [xstart, xend, ystart, yend] = range
-    for(let i = xstart; i < xend; i += 1) {
-      for(let j = ystart; j < yend; j += 1) {
-        sprites.drawTile(tile, context, i, j)
-      }
-    }
-  }
-}
-
 
--- src/loaders.ts
+++ src/loaders.ts
@@ -1,4 +1,5 @@
-import { LevelDefinition } from "./types.js"
+import SpriteSheet from "./sprite.js"
+import { Background, LevelDefinition } from "./types.js"
 
 export function loadAsset({
   name,
@@ -30,4 +31,56 @@ export async function loadLevel({
   const response = await fetch(url)
   const json = await response.json()
   return json as LevelDefinition
+}
+
+export async function loadBackgroundSprites({
+  asset,
+  levelName,
+  canvas,
+}: {
+  asset: string
+  levelName: string
+  canvas: HTMLCanvasElement
+}): Promise<[SpriteSheet, LevelDefinition]> {
+
+  const context = canvas.getContext('2d')
+  if (!context) {
+    throw Error('could not create 2d context?!')
+  }
+  const [loadedAsset, level] = await Promise.all([
+    loadAsset({ name: asset }),
+    loadLevel({ name: levelName })
+  ])
+  const sprites = new SpriteSheet(loadedAsset, 16, 16)
+  sprites.define('ground', 0, 0)
+  sprites.define('sky', 3, 23)
+  for(const background of level.backgrounds) {
+    drawBackground({
+      background,
+      sprites,
+      context, 
+    })
+  }
+  return [sprites, level]
+}
+
+function drawBackground({
+  background,
+  context,
+  sprites,
+}: {
+  background: Background
+  context: CanvasRenderingContext2D
+  sprites: SpriteSheet
+}) {
+
+  const { tile, ranges } = background
+  for(const range of ranges) {
+    const [xstart, xend, ystart, yend] = range
+    for(let i = xstart; i < xend; i += 1) {
+      for(let j = ystart; j < yend; j += 1) {
+        sprites.drawTile(tile, context, i, j)
+      }
+    }
+  }
 }
